7a9b5c788f930ce74ac82033a9d5c609b28fa8ec
stephane de la torre <s.dlt@hotmail.fr>
2018-03-17 19:55:40 +0100

    session 3/26 done

--- index.js
+++ index.js
@@ -4,16 +4,30 @@ const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
+const osx = process.platform === "darwin";
+
 const menuTemplate = [
   {
     label: "File",
     submenu: [
       {
-        label: "New Todo"
+        label: "New Todo",
+        accelerator: osx ? "cmd+n" : "ctrl+n"
+      },
+      {
+        label: "Quit",
+        accelerator: osx ? "cmd+q" : "ctrl+q",
+        click() {
+          app.quit();
+        }
       }
     ]
   }
 ];
+
+if (osx) {
+  menuTemplate.unshift({});
+}
 
 app.on("ready", () => {
   mainWindow = new BrowserWindow({});

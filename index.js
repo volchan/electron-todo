7ecc1518f53ba26186243ebdb0b7e46c2a8440dc
stephane de la torre <s.dlt@hotmail.fr>
2018-03-17 22:14:36 +0100

    session 3/29 done

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ if (osx) {
 app.on("ready", () => {
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+  mainWindow.on("closed", () => app.quit());
 
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
